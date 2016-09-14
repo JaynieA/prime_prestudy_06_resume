2c51c319dede0e15e6b7b22be50e853ca2f51947
Jaynie <jaynie.lunger@gmail.com>
2016-09-14 14:14:54 -0500

    got rid of extra tab, changed education icon to grad cap

--- js/tabs.js
+++ js/tabs.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	 		$(tab1).html("<span class='glyphicon glyphicon-user'></span>");
 	 		$(tab2).html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
 	 		$(tab3).html("<i class='fa fa-university' aria-hidden='true'></i>");
-	 		$(tab4).html("<i class='fa fa-book' aria-hidden='true'></i>");
+	 		$(tab4).html("<i class='fa fa-mortar-board' aria-hidden='true'></i>");
 	 	}
 		//Control tabs if window resized to smaller than 700 px width
 	 	$(window).resize(function() {
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	 			$(tab1).html("<span class='glyphicon glyphicon-user'></span>");
 		 		$(tab2).html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
 		 		$(tab3).html("<i class='fa fa-university' aria-hidden='true'></i>");
-		 		$(tab4).html("<i class='fa fa-book' aria-hidden='true'></i>");
+		 		$(tab4).html("<i class='fa fa-mortar-board' aria-hidden='true'></i>");
 	 		}
 	 		if (win.width() >= 700) { 
 	 			$(tab1).html("About Me");
