2a075551fcd6ae6f4ddc86d2382bfe4439872464
Jaynie <jaynie.lunger@gmail.com>
2016-09-14 00:04:14 -0500

    added script to swap tab text with icons on window resize

--- js/tabs.js
+++ js/tabs.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
-	//Control tabs if window loaded on screen width <700 px
-	var win = $(this); //this = window
- 	if (win.width() <= 700) { 
- 		console.log("window is loaded under 700 px");
- 	}
-	//Control tabs if window resized to smaller than 700 px width
- 	$(window).resize(function() {
- 		if (win.width() <= 700) { 
- 			console.log("window has been resized to under 700 px");
- 		}
- 	});
+	function changeTabText() {
+		//Control tabs if window loaded on screen width <700 px
+		var win = $(this); //this = window
+	 	if (win.width() <= 700) { 
+	 		$('#tab1').html("<span class='glyphicon glyphicon-user'></span>");
+	 		$('#tab2').html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
+	 		$('#tab3').html("<i class='fa fa-university' aria-hidden='true'></i>");
+	 		$('#tab4').html("<i class='fa fa-book' aria-hidden='true'></i>");
+	 	}
+		//Control tabs if window resized to smaller than 700 px width
+	 	$(window).resize(function() {
+	 		if (win.width() <= 700) { 
+	 			$('#tab1').html("<span class='glyphicon glyphicon-user'></span>");
+		 		$('#tab2').html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
+		 		$('#tab3').html("<i class='fa fa-university' aria-hidden='true'></i>");
+		 		$('#tab4').html("<i class='fa fa-book' aria-hidden='true'></i>");
+	 		}
+	 		if (win.width() >= 700) { 
+	 			$('#tab1').html("About Me");
+		 		$('#tab2').html("Employment");
+		 		$('#tab3').html("Professional Development");
+		 		$('#tab4').html("Education");
+	 		}
+	 	});
+	}
+	
+	changeTabText();
 });
 
