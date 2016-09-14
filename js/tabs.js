b02208b334e51bc48c01b5ae2c8b4ceb67a488c6
Jaynie <jaynie.lunger@gmail.com>
2016-09-14 00:07:07 -0500

    declared variables in changeTabText function

--- js/tabs.js
+++ js/tabs.js
@@ -2,25 +2,29 @@ $(document).ready(function() {
 	function changeTabText() {
 		//Control tabs if window loaded on screen width <700 px
 		var win = $(this); //this = window
+		var tab1 = $('#tab1');
+		var tab2 = $('#tab2');
+		var tab3 = $('#tab3');
+		var tab4 = $('#tab4');
 	 	if (win.width() <= 700) { 
-	 		$('#tab1').html("<span class='glyphicon glyphicon-user'></span>");
-	 		$('#tab2').html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
-	 		$('#tab3').html("<i class='fa fa-university' aria-hidden='true'></i>");
-	 		$('#tab4').html("<i class='fa fa-book' aria-hidden='true'></i>");
+	 		$(tab1).html("<span class='glyphicon glyphicon-user'></span>");
+	 		$(tab2).html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
+	 		$(tab3).html("<i class='fa fa-university' aria-hidden='true'></i>");
+	 		$(tab4).html("<i class='fa fa-book' aria-hidden='true'></i>");
 	 	}
 		//Control tabs if window resized to smaller than 700 px width
 	 	$(window).resize(function() {
 	 		if (win.width() <= 700) { 
-	 			$('#tab1').html("<span class='glyphicon glyphicon-user'></span>");
-		 		$('#tab2').html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
-		 		$('#tab3').html("<i class='fa fa-university' aria-hidden='true'></i>");
-		 		$('#tab4').html("<i class='fa fa-book' aria-hidden='true'></i>");
+	 			$(tab1).html("<span class='glyphicon glyphicon-user'></span>");
+		 		$(tab2).html("<i class='fa fa-suitcase' aria-hidden='true'></i>");
+		 		$(tab3).html("<i class='fa fa-university' aria-hidden='true'></i>");
+		 		$(tab4).html("<i class='fa fa-book' aria-hidden='true'></i>");
 	 		}
 	 		if (win.width() >= 700) { 
-	 			$('#tab1').html("About Me");
-		 		$('#tab2').html("Employment");
-		 		$('#tab3').html("Professional Development");
-		 		$('#tab4').html("Education");
+	 			$(tab1).html("About Me");
+		 		$(tab2).html("Employment");
+		 		$(tab3).html("Professional Development");
+		 		$(tab4).html("Education");
 	 		}
 	 	});
 	}
